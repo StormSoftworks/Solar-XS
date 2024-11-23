c51dda8b0981b5762f96cbb7d6d09746927a5a82
Storm Softworks <106533596+StormSoftworks@users.noreply.github.com>
2024-11-23 20:51:24 +0200

    Update Enviroment.lua

--- Enviroment.lua
+++ Enviroment.lua
@@ -4,3 +4,4 @@ function printidentity() print('Current identity is ' .. identity) end
 function identifyexecutor() return 'Solar-XS' end
 function getthreadidentity() return identity end
 function setthreadidentity(v) identity = v end
+function saveInstance() return loadstring(game:HttpGet("https://raw.githubusercontent.com/luau/SynSaveInstance/main/saveinstance.luau", true), "saveinstance")()({}) end
