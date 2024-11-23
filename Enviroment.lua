f9eec9c8fe59509099be35dfc8d2f5b27cbf4264
Storm Softworks <106533596+StormSoftworks@users.noreply.github.com>
2024-11-23 20:44:50 +0200

    Update Enviroment.lua

--- Enviroment.lua
+++ Enviroment.lua
@@ -1,2 +1,3 @@
 function printidentity() print('Current identity is 7') end
-function identifyexecutor() return 'Solar-XS', '1.1' end
+function identifyexecutor() return 'Solar-XS' end
+function getthreadidentity() return '7' end
